7a6c4b53847ddd380cddf310e76cec0bff7518c0
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2022-05-10 13:14:18 -0600

    Errors And Exceptions

---
+++ github.com/saragoodhue/ErrorsAndExceptionPractice.git/Errors And Exception Practice/src/ErrorsExceptions.java
@@ -0,0 +1,22 @@
+
+public class ErrorsExceptions
+	{
+
+		public static void main(String[] args)
+			{
+				int nums[ ] = new int [4];
+				try
+					{
+					nums[7] = 10;
+					System.out.println("This line won’t print.");
+			
+					{
+					System.out.println("Oops.  Index is out of bounds.");
+					}
+
+
+			}
+		
+		
+
+	}
